42fe218dd3b091a3745e123d50d647c9f754b878
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-04-27 16:12:38 +0200

    Knap styling

--- src/gui/controllers/ProjectsController.java
+++ src/gui/controllers/ProjectsController.java
@@ -1,20 +1,24 @@
 package gui.controllers;
 
+import be.Enum.SystemRole;
 import be.Project;
+import com.jfoenix.controls.JFXButton;
+import gui.util.NodeAccessLevel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import util.ViewPaths;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class ProjectsController extends BaseController implements Initializable {
     @FXML
@@ -25,11 +29,76 @@ public class ProjectsController extends BaseController implements Initializable 
     private TableColumn<Project, String> tcClient, tcLocation, tcProjectName;
     @FXML
     private TableColumn<Project, Date> tcCreated;
+    private NodeAccessLevel buttonAccessLevel;
 
     private ObservableList<Project> allProjects = FXCollections.observableList(new ArrayList<>()); //TODO Slet, testing
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        loadTableView();
+        addLoadedButtons();
+
+    }
+
+    private void addLoadedButtons() {
+        initializeButtonAccessLevels();
+
+        try {
+            //Gets the logged-in user's role
+            SystemRole loggedInUserRole = getModelsHandler()
+                    .getSystemUserModel()
+                    .getLoggedInSystemUser()
+                    .getValue()
+                    .getRole();
+
+            // Loops through the buttons and adds them to the sidebar if the user has the right access level
+            for (Node button : buttonAccessLevel.getNodes()) {
+
+                List<SystemRole> accessLevel = buttonAccessLevel.getAccessLevelsForNode(button);
+
+                if(accessLevel.contains(loggedInUserRole)) addButton((Button) button);
+            }
+        } catch (Exception e) {
+            displayError(e);
+        }
+    }
+
+    private void addButton(Button button) {
+        buttonArea.getChildren().add(0, button);
+    }
+
+    private void initializeButtonAccessLevels() {
+        buttonAccessLevel = new NodeAccessLevel();
+
+        //TODO Slet, testing
+        buttonAccessLevel.addNodeAccessLevel(
+                loadButton("👥 Users", ViewPaths.USERS_VIEW),
+                Arrays.asList(SystemRole.Administrator));
+
+        buttonAccessLevel.addNodeAccessLevel(
+                loadButton("👥 Users", ViewPaths.USERS_VIEW),
+                Arrays.asList(SystemRole.Administrator));
+
+        buttonAccessLevel.addNodeAccessLevel(
+                loadButton("👥 Users", ViewPaths.USERS_VIEW),
+                Arrays.asList(SystemRole.Administrator));
+
+        buttonAccessLevel.addNodeAccessLevel(
+                loadButton("👥 Users", ViewPaths.USERS_VIEW),
+                Arrays.asList(SystemRole.Administrator));
+    }
+
+    private Button loadButton(String text, String fxmlPath) {
+        JFXButton button = new JFXButton(text);
+        button.setFont(Font.font(16));
+        button.setPrefWidth(150);
+        button.setPrefHeight(60);
+        //button.setOnAction(e -> mainBorderPane.setCenter(loadView(fxmlPath).getRoot()));
+
+        return button;
+    }
+
+    private void loadTableView() {
         tcClient.setCellValueFactory(new PropertyValueFactory<>("client"));
         tcLocation.setCellValueFactory(new PropertyValueFactory<>("location"));
         tcProjectName.setCellValueFactory(new PropertyValueFactory<>("projectName"));
