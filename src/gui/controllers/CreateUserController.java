5d222c9f4ef230d18c0847ad7725ee407d8d7e22
JennyTromTheBlock <kenmad01@easv365.dk>
2023-05-01 10:14:26 +0200

    test data

--- src/gui/controllers/CreateUserController.java
+++ src/gui/controllers/CreateUserController.java
@@ -1,22 +1,33 @@
 package gui.controllers;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CreateUserController {
+public class CreateUserController extends BaseController implements Initializable {
 
 
     public VBox loginInformation;
-
+    public ComboBox cbRoles;
 
 
     public void handleBack(ActionEvent actionEvent) {
     }
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {//todo test content 
+            cbRoles.setItems(getModelsHandler().getSystemUserModel().getAllUsers());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        AutoCompleteBox a = new AutoCompleteBox(cbRoles);
+    }
 }
