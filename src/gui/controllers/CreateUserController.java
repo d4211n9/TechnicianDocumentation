df870e0b536476fb646a682dff7bdc37d3a218b6
JennyTromTheBlock <kenmad01@easv365.dk>
2023-05-01 12:01:14 +0200

    ad

--- src/gui/controllers/CreateUserController.java
+++ src/gui/controllers/CreateUserController.java
@@ -18,7 +18,7 @@ public class CreateUserController extends BaseController implements Initializabl
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        try {//todo test content 
+        try {
             cbRoles.setItems(getModelsHandler().getSystemUserModel().getAllRoles());
         } catch (Exception e) {
             throw new RuntimeException(e);
