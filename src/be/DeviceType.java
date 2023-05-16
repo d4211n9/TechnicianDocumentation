00afe92e697dd655e0787938bdc17f62b73ee1ff
Kaosk12 <84007527+Kaosk12@users.noreply.github.com>
2023-05-16 12:14:10 +0200

    DeviceType BE

---
+++ src/be/DeviceType.java
@@ -0,0 +1,25 @@
+package be;
+
+public class DeviceType {
+    private String name;
+    private String imagePath;
+    private boolean hasLoginDetails;
+
+    public DeviceType(String name, String imagePath, boolean hasLoginDetails) {
+        this.name = name;
+        this.imagePath = imagePath;
+        this.hasLoginDetails = hasLoginDetails;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public boolean isHasLoginDetails() {
+        return hasLoginDetails;
+    }
+}
