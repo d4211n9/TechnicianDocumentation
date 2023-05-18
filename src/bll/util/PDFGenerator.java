ddb521fdf7106ab490c832bbf560f7dce87e426c
Kaosk12 <84007527+Kaosk12@users.noreply.github.com>
2023-05-18 21:02:51 +0200

    PDF setup

--- src/bll/util/PDFGenerator.java
+++ src/bll/util/PDFGenerator.java
@@ -1,6 +1,7 @@
 package bll.util;
 
 import be.Client;
+import be.Installation;
 import be.Project;
 import bll.managers.ProjectManager;
 import com.itextpdf.io.image.ImageData;
@@ -20,7 +21,11 @@ import com.itextpdf.layout.borders.DashedBorder;
 import com.itextpdf.layout.borders.SolidBorder;
 import com.itextpdf.layout.element.*;
 import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.properties.HorizontalAlignment;
+import com.itextpdf.layout.properties.TextAlignment;
 import com.itextpdf.layout.properties.UnitValue;
+import com.itextpdf.layout.properties.VerticalAlignment;
+import com.itextpdf.layout.splitting.BreakAllSplitCharacters;
 import exceptions.BLLException;
 import util.SymbolPaths;
 import com.itextpdf.io.font.constants.StandardFonts;
@@ -45,8 +50,11 @@ public class PDFGenerator {
 
             document = new Document(pdfDocument);
 
+            document.setFont(getFont());
+
             addLogo(document);
             addClientInfo(document, project.getClient());
+            addProjectInfo(document, project);
         }
         catch (Exception e) {
             BLLException bllException = new BLLException("Failed to generate project PDF", e);
@@ -62,31 +70,58 @@ public class PDFGenerator {
         }
     }
 
-    private static void addClientInfo(Document document, Client client) {
-        Paragraph clientInfo = new Paragraph("Client Info");
+    private static void addProjectInfo(Document document, Project project) throws Exception {
+        Paragraph projectInfo = new Paragraph("Project Info").setFont(getFontBold());
+        projectInfo.setMarginTop(getMargin());
+        document.add(projectInfo);
+
+        Table projectTable = new Table(UnitValue.createPercentArray(new float[] {5f, 5f})).useAllAvailableWidth();
+        projectTable.setBorder(new SolidBorder(new DeviceRgb(0f, 0f, 0f), 1f));
+
+        Paragraph projectName = getParagraph("Name: ", getFontBold());
+        projectName.add(getParagraph(project.getName(), getFont()));
+
+        Paragraph projectAddress = getParagraph("Address: ", getFontBold());
+        projectAddress.add(getParagraph(project.getAddress().getStreet() + ", " + project.getAddress().getPostalCode() + project.getAddress().getCity(), getFont()));
+
+        Paragraph projectDescription = getParagraph("Description: ", getFontBold());
+        projectDescription.add(getParagraph(project.getDescription(), getFont()));
+
+        Paragraph projectCreated = getParagraph("Created: ", getFontBold());
+        projectCreated.add(getParagraph(project.getCreated().toString(), getFont()));
+
+        projectTable.addCell(getNoBorderedTableCell(projectName));
+        projectTable.addCell(getNoBorderedTableCell(projectAddress));
+        projectTable.addCell(getNoBorderedTableCell(projectDescription));
+        projectTable.addCell(getNoBorderedTableCell(projectCreated));
+
+        document.add(projectTable);
+    }
+
+    private static void addClientInfo(Document document, Client client) throws Exception {
+        Paragraph clientInfo = new Paragraph("Client Info").setFont(getFontBold());
+        clientInfo.setMarginTop(getMargin());
         document.add(clientInfo);
 
         Table clientTable = new Table(UnitValue.createPercentArray(new float[] {5f, 5f})).useAllAvailableWidth();
-
         clientTable.setBorder(new SolidBorder(new DeviceRgb(0f, 0f, 0f), 1f));
 
-        Paragraph clientName = new Paragraph("Name: " + client.getName());
-        Paragraph clientPhone = new Paragraph("Phone: " + client.getPhone());
-        Paragraph clientLocation = new Paragraph("Location: " + "\n" + client.getStreet() + "," + "\n" + client.getPostalCode() + " " + client.getCity());
-        Paragraph clientEmail = new Paragraph("Email: " + client.getEmail());
-        clientTable.addCell(new Cell()
-                .add(clientName)
-                .setBorder(Border.NO_BORDER));
-        clientTable.addCell(new Cell()
-                .add(clientPhone)
-                .setBorder(Border.NO_BORDER));
-        clientTable.addCell(
-                new Cell()
-                    .add(clientLocation)
-                    .setBorder(Border.NO_BORDER));
-        clientTable.addCell(new Cell()
-                .add(clientEmail)
-                .setBorder(Border.NO_BORDER));
+        Paragraph clientName = getParagraph("Name: ", getFontBold());
+        clientName.add(getParagraph(client.getName(), getFont()));
+
+        Paragraph clientPhone = getParagraph("Phone: ", getFontBold());
+        clientPhone.add(getParagraph(client.getPhone(), getFont()));
+
+        Paragraph clientLocation = getParagraph("Location: ", getFontBold());
+        clientLocation.add(getParagraph(client.getStreet() + ", " + client.getPostalCode() + " " + client.getCity(), getFont()));
+
+        Paragraph clientEmail = getParagraph("Email: ", getFontBold());
+        clientEmail.add(getParagraph(client.getEmail(), getFont()));
+
+        clientTable.addCell(getNoBorderedTableCell(clientName));
+        clientTable.addCell(getNoBorderedTableCell(clientPhone));
+        clientTable.addCell(getNoBorderedTableCell(clientLocation));
+        clientTable.addCell(getNoBorderedTableCell(clientEmail));
 
         document.add(clientTable);
     }
@@ -101,12 +136,24 @@ public class PDFGenerator {
         document.add(logoImage);
     }
 
+    private static Cell getNoBorderedTableCell(Paragraph paragraph) {
+        return new Cell().add(paragraph).setBorder(Border.NO_BORDER);
+    }
+
+    private static Paragraph getParagraph(String text, PdfFont font) {
+        return new Paragraph(text).setFont(font).setSplitCharacters(new BreakAllSplitCharacters());
+    }
+
     private static PdfFont getFontBold() throws Exception {
-        return PdfFontFactory.createFont(StandardFonts.TIMES_BOLD);
+        return PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
     }
 
     private static PdfFont getFont() throws Exception {
-        return PdfFontFactory.createFont(StandardFonts.TIMES_ROMAN);
+        return PdfFontFactory.createFont(StandardFonts.HELVETICA);
+    }
+
+    private static float getMargin()  {
+        return 20f;
     }
 
     public static void main(String[] args) {
