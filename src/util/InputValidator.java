ae0feba855cf07dcf6697cc14af2186f1c569d2f
steffaneckeroth <stefeckeroth@hotmail.com>
2023-05-21 23:49:45 +0200

    Unit test inputvalidator

--- src/util/InputValidator.java
+++ src/util/InputValidator.java
@@ -2,6 +2,7 @@ package util;
 
 public class InputValidator {
     public static boolean isEmail(String email) {
+
         return email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}") && !email.isEmpty();
     }
 
